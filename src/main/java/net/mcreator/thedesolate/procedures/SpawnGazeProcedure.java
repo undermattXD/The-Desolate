7881ed20f77cc13c337ad58e35466aa82d747e0b
Jimisslim <129909191+Jimisslim@users.noreply.github.com>
2026-08-25 12:46:48 -0700

    fianle

--- src/main/java/net/mcreator/thedesolate/procedures/SpawnGazeProcedure.java
+++ src/main/java/net/mcreator/thedesolate/procedures/SpawnGazeProcedure.java
@@ -34,7 +34,7 @@ public class SpawnGazeProcedure {
 			return;
 		if (!(world instanceof Level _lvl0 && _lvl0.isBrightOutside())) {
 			if (TheDesolateModVariables.MapVariables.get(world).Has_Gaze_sapwned_tonight == false) {
-				if (Math.random() <= 1) {
+				if (Math.random() <= 0.001) {
 					if (world instanceof ServerLevel _level) {
 						Entity entityToSpawn = TheDesolateModEntities.RUPTURED_GAZE.get().spawn(_level,
 								BlockPos.containing(entity.getX() + entity.getLookAngle().x * (-15),
