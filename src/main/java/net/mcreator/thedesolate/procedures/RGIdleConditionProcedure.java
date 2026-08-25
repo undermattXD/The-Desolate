f2019a767d44ca3499df04dd14543e7e6b74b23e
Jimisslim <129909191+Jimisslim@users.noreply.github.com>
2026-08-24 17:28:02 -0700

    Fixanims

--- src/main/java/net/mcreator/thedesolate/procedures/RGIdleConditionProcedure.java
+++ src/main/java/net/mcreator/thedesolate/procedures/RGIdleConditionProcedure.java
@@ -6,7 +6,7 @@ public class RGIdleConditionProcedure {
 	public static boolean execute(Entity entity) {
 		if (entity == null)
 			return false;
-		if (entity.getDeltaMovement().x() == 0 && entity.getDeltaMovement().z() == 0 && entity.getDeltaMovement().y() == 0) {
+		if (entity.getDeltaMovement().x() <= 0.5 && entity.getDeltaMovement().z() <= 0.5 && entity.getDeltaMovement().y() <= 0.5) {
 			return true;
 		}
 		return false;
